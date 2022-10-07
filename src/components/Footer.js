762997b47858c7be1c154ced6019cdbec5827f10
HudaHajira4 <hudahajira744@gmail.com>
2022-10-07 11:15:02 +0530

    gitd1

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <div className="footer">
+            <h5>&copy; Copyright Code For India. All rights reserved.</h5>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,4 @@
+{/* <div className="header">
+    <h1>My Website</h1>
+    <p>With a <b>flexible</b> layout.</p>
+</div> */}
